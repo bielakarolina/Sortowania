fe641a60c685925044fa462d8adc0538d5c69d7b
bielakarolina <bielakarolina77gmail.com>
2018-03-21 19:59:22 +0100

    nowe

---
+++ Comparison.cpp
@@ -0,0 +1,82 @@
+//
+// Created by root on 21.03.18.
+//
+
+
+#include <iostream>
+#include <stack>
+#include <fstream>
+using namespace std;
+
+const int n =10;
+
+
+
+int Partition2(int A[], int p, int r) // p - początek przedziału, r = koniec
+{
+    int x = A[r]; // x to ostatni element
+    int i = p - 1; // i ustawiamy przed przedziałem
+    int j;
+    for (j = p; j < r; j++) {
+        if (A[j] <= x) // elementy mniejsze od x są przekładane na początek
+        {
+            i++;
+            swap(A[i], A[j]);
+        }
+    }
+    swap(A[r], A[i + 1]);
+    return i + 1;
+}
+
+
+void QuickSort(int t[], int p, int r)
+{
+    if(p<r)
+    {
+        int q=Partition2(t,p,r);
+        QuickSort(t,p,q-1); // lub dla odpowiedniego partition QuickSort(t,p,q-1)
+        QuickSort(t,q+1,r);
+    }
+
+}
+
+void print_t(int A[],int s){
+    for(int i=0;i<s;i++){
+        cout<<A[i]<<" ";
+    }
+    cout<<endl;
+}
+
+void generate_data(int N){
+    //N ilość danych ktore będziemy generować
+
+    srand(time(0));
+    fstream plik( "dane.txt", ios::out );
+    if( plik.good() )
+    {
+        for( int i = 1; i <= N; i++ )
+        {
+            plik << rand() % 5000  + 1 << ", ";
+            plik.flush();
+        }
+        plik.close();
+    }
+
+}
+
+
+
+int main()
+{
+
+
+
+    generate_data(1000);
+    cout<<"Sortowanie tablicy QuickSort"<<endl;
+    int n=10;
+    int T[n]={5, 10, 0, 20, 15, 35, 25, 30, 46, 1};
+    print_t(T,n);
+    QuickSort(T,0,n);
+    print_t(T,n);
+    return 0;
+}
