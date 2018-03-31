52eb68acf837e89f2b48207dcf6ba1ae88a052c6
bielakarolina <bielakarolina77@gmail.com>
2018-03-31 22:32:37 +0200

    Kolosy

--- .idea/Kolokwium2015_16A.cpp
+++ .idea/Kolokwium2015_16A.cpp
@@ -1,6 +1,9 @@
 //
 // Created by Karolina on 31.03.2018.
 //
+#include <iostream>
+#include <string>
+using namespace std;
 
 //1. Proszę zaimplementować funkcję void SumSort(int A[], int B[], int n).
 //Funkcja ta przyjmuje na wejściu dwie n 2 -elementowe tablice (A i B) i
@@ -9,6 +12,44 @@
 //Proszę zaimplementować funkcję SumSort tak, by działała możliwie jak najszybciej.
 //Proszę oszacować i podać jej złożoność czasową.
 //
+struct sum{
+    int x;
+    int val;
+};
+void SumSort(int A[], int B[], int n) {
+    sum * sums = new sum[n];
+    for (int i = 0; i < n; i++) {
+        sums[i].x = i*n;
+        sums[i].val = 0;
+        for (int j = 0; j < n; j++) sums[i].val += A[i*n + j];
+    }
+    print2(sums, n);
+
+    sum * sumsS = new sum[n];
+    int max, min;
+    max = min = sums[0].val;
+    for (int i = 0; i < n; i++) {
+        if (sums[i].val > max) max = sums[i].val;
+        if (sums[i].val < min) min = sums[i].val;
+    }
+
+    int k = (max - min) + 1;
+    int * C = new int[k];
+    for (int i = 0; i < k; i++) C[i] = 0;
+    for (int i = 0; i < n; i++) C[sums[i].val - min]++;
+    for (int i = 0; i < k - 1; i++) C[i + 1] += C[i];
+    for (int i = 0; i < n; i++) sumsS[--C[sums[i].val - min]] = sums[i];
+    for (int i = 0; i < n; i++) {
+        int b = sumsS[i].x;
+        for (int j = 0; j < n; j++) B[i*n + j] = A[j+b];
+    }
+
+    delete[] sums;
+    delete[] sumsS;
+    delete[] C;
+}
+
+
 //2. Dana jest n elementowa tablica A zawierająca liczby naturalne (potencjalnie bardzo duże).
 //Wiadomo, że tablica A powstała w dwóch krokach.
 //Najpierw wygenerowano losowo (z nieznanym rozkładem) n różnych liczn nieparzystych i
