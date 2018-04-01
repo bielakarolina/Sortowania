353c087a5e9dcef1ea6e4454a31b8ca88ace0f67
bielakarolina <bielakarolina77@gmail.com>
2018-04-01 20:19:06 +0200

    Kolosy

--- Kolokwium2012_2013.cpp
+++ Kolokwium2012_2013.cpp
@@ -10,6 +10,7 @@
 //struct Node { Node* next; int val; }; Node* QuickerSort ( Node* head )
 //Argumentem funkcji jest wskaźnik na głowę listy do posortowania a wynikiem powinien być wskaźnik na głowę
 //listy posortowanej. Sortowanie powinno polegać na porównywaniu wartości val list oraz przepinaniu wskaźników next.
+
 //2. Proszę zaprojektowac strukturę danych przechowującą liczby i pozwalającą na następujące operacje
 //(zakładamy, że wszystkie liczby umieszczane w strukturze są różne): Init(n).
 //Tworzy zadaną strukturę danych zdolną pomieścić maksymalnie n liczb. Insert(x).
@@ -18,6 +19,10 @@
 //        złożoność O(log n, gdzie n to ilość liczb znajdujących się obecnie w strukturze. W tym zadaniu nie trzeba
 //        implementować podanych operacji, a jedynie przekonująco opisać jak powinny być zrealizowane i dlaczego mają
 //wymaganą złożoność.
+// ta struktura byłaby podobna do drzewa binarnego, którego wysokość wynosi logn
+//dlatego inicializowanie drzewa, operacja insert będą wynosiły logn
+//jeżelu będziemy chcieli odnaleść max lub min ,przejdziemy do najbardziej na prawo, najdalszego prawego syna,lub lewo..,
+//czyli do max wysokości drzewa czyli logn+ naprawa drzewa logn
 //3. Proszę napisać funkcję bool possible( char* u, char* v, char* w ), która zwraca prawdę jeśli z liter słów u i v
 //        da się ułożyć słowo w (nie jest konieczne wykorzystanie wszystkich liter) oraz fałsz w przeciwnym wypadku.
 //Można założyć, że w i v składają się wyłącznie z małych liter alfabetu łacińskiego. Proszę krótko uzasadnić wybór
