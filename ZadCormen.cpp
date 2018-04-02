2cc6638105d11c020dcb1a1cc5a7f4ff849952f0
bielakarolina <bielakarolina77@gmail.com>
2018-04-03 00:29:13 +0200

    Kolosy

---
+++ ZadCormen.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Karolina on 03.04.2018.
+//
+
+//1.Podaj lagorytm scalający k posortowanych list gdzie n jest łączną liczba elementów w listach O(nlogk)
+//
+//2.Heap-Delete(A,i) usuwa wartość z węłza i w kopcu A. O(logn) dla n elementowego kopca
+
+//3.Inwersje
+//Niech A[n] będzie tablicą zawierającą n różnych liczb, Jesli i<j oraz A[i]>A[j] to para (i,j) jest inwersja w A
+//w czasie O(nlogn)
+//Policz liczbe inwersji w A
+
+//4. Problem maxymalnej podtablicy 69 strona w Cormenie
+//5. Heap Extract_MIN, insert, increse-key, decrease-key,
+//6.d-kopce, podaj operacje insert ,extract_max
+//7.Zaprojektuj algorytm, który dla danych n liczb z przedziału od 0 do k, wykonuje wstępne obliczenia a następnie na ich
+//podstawie pozwala w czasie O(1) określić, ile spośród danych n liczb leżu w przedziale [a...b]
+//Zł: O(n+k)
+//8. Skonsturłuj algo, który pozwala posortować n liczb całkowitych z przedziału 0 do n^3-1 w czasie O(n)
