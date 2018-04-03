e9805c7c8a5802e0516f93687a6240455b7a6949
basiachra <basia.chra@gmail.com>
2018-04-03 19:11:07 +0200

    working every time inversions

--- inwersje.cpp
+++ inwersje.cpp
@@ -2,7 +2,7 @@
 #include<algorithm>
 using namespace std;
 int x = 0;
-int merge(int tab[], int left1, int m, int right2, int n){
+void merge(int tab[], int left1, int m, int right2, int n){
 	
 	int pivot = left1;
 	int * Tab = new int[n];
@@ -14,9 +14,12 @@ int merge(int tab[], int left1, int m, int right2, int n){
 	while(left1<= right1 && left2 <= right2){
 		if(tab[left1] < tab[left2]){
 			Tab[i] = tab[left1++];
-			licznik++;
+			//licznik++;
 		}
-		else Tab[i] = tab[left2++];
+		else {
+			Tab[i] = tab[left2++];
+			x+= left1;
+		}
 		i++;
 	}	
 	
@@ -24,7 +27,6 @@ int merge(int tab[], int left1, int m, int right2, int n){
 	while(left2 <= right2) Tab[i++] = tab[left2++];
 	
 	for(int i =pivot; i<=right2; i++) tab[i] = Tab[i];
-	return licznik;
 }
 
 void mergeSort(int tab[], int left, int right, int n){
@@ -33,7 +35,7 @@ void mergeSort(int tab[], int left, int right, int n){
 		int m =(left + right)/2;
 		mergeSort(tab,left,m,n);
 		mergeSort(tab,m+1,right,n);
-		x+=merge(tab, left, m, right, n);	
+		merge(tab, left, m, right, n);	
 	}
 	
 }
@@ -41,10 +43,10 @@ void mergeSort(int tab[], int left, int right, int n){
 
 int main(){
 	
-	int tab[7] = {5,2,4,8,6,3,7};
-	mergeSort(tab,0,6,7);
+	int tab[6] = {1,2,5,3,4,0};
+	mergeSort(tab,0,5,6);
 	
-	for(int i =0; i<7; i++){
+	for(int i =0; i<6; i++){
 		cout<<tab[i]<<" ";
 	}
 	cout<<endl;
